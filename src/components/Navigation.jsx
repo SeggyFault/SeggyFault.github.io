b86cc2b995e784271f8b6c972dafba99ec451a6e
sggyflt <sgflt88@gmail.com>
2024-12-15 15:16:19 +0100

    feat: add Russo and Lato Google fonts

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,16 +8,16 @@ const Navigation = () => {
         className="w-16"/>
       </a>
       <ul className="flex items-center justify-between w-1/2">
-        <li className="text-base text-white uppercase hover:underline">
+        <li className="text-base text-white uppercase font-lato hover:underline">
           <a href="#skills">Skills</a>
         </li>
-        <li className="text-base text-white uppercase hover:underline">
+        <li className="text-base text-white uppercase font-lato hover:underline">
           <a href="#projects">Projects</a>
         </li>
-        <li className="text-base text-white uppercase hover:underline">
+        <li className="text-base text-white uppercase font-lato hover:underline">
           <a href="#about">About</a>
         </li>
-        <li className="text-base text-white uppercase hover:underline">
+        <li className="text-base text-white uppercase font-lato hover:underline">
           <a href="#contact">Contact</a>
         </li>
       </ul>
