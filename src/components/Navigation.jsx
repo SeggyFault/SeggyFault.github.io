91406d7457ec6e397997aa4b0c9d9e84913a1a4f
sggyflt <sgflt88@gmail.com>
2024-12-15 19:12:15 +0100

    feat: adjust Header for smaller screens

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,3 +1,5 @@
+import { MdMenu } from "react-icons/md";
+
 const Navigation = () => {
   return (
     <nav className="flex items-center justify-between w-full max-w-[1140px] my-0 mx-auto px-5 py-5 z-10 relative">
@@ -7,7 +9,10 @@ const Navigation = () => {
         alt="rat icon" 
         className="w-12"/>
       </a>
-      <ul className="flex items-center justify-between w-1/2">
+
+      <button className="text-5xl text-white md:hidden"><MdMenu/></button>
+
+      <ul className="hidden w-1/2 md:flex md:items-center md:justify-between">
         <li className="text-base text-white uppercase font-lato hover:underline">
           <a href="#skills">Skills</a>
         </li>
