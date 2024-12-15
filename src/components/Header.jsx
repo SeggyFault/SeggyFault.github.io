6aabb2d7dcfa3c612b015862d5a2c049e357afd7
sggyflt <sgflt88@gmail.com>
2024-12-15 14:23:48 +0100

    feat: add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = ({ children }) => {
+  return (
+    <header className="bg-[url('./src/assets/images/hero-image.jpg')] bg-cover bg-center bg-no-repeat min-h-screen"></header>
+  );
+};
+
+export default Header;
