7f4a4ea062b2a9d445bafcfd9cbd0a203f9f6d91
sggyflt <sgflt88@gmail.com>
2024-12-20 20:14:05 +0100

    fix: move skillsData to a separate file

---
+++ src/data/skillsData.js
@@ -0,0 +1,57 @@
+const skillsData = [
+  {
+    title: "The Heavy Arsenal",
+    skills: [
+      {
+        image: "/assets/images/html5.svg",
+        title: "HTML5",
+      }, 
+      {
+        image: "/assets/images/css3.svg",
+        title: "CSS3",
+      }, 
+      {
+        image: "/assets/images/es6.svg",
+        title: "ES6",
+      }, 
+      {
+        image: "/assets/images/reactjs.svg",
+        title: "React.js",
+      },
+      {
+        image: "/assets/images/git.svg",
+        title: "Git",
+      },
+      {
+        image: "/assets/images/nodejs.svg",
+        title: "Node.js",
+      },
+      {
+        image: "/assets/images/mongodb.svg",
+        title: "MongoDB",
+      }, 
+      {
+        image: "/assets/images/figma.svg",
+        title: "Figma",
+      },
+      {
+        image: "/assets/images/vscode.svg",
+        title: "VSCode",
+      },
+      {
+        image: "/assets/images/sass.svg",
+        title: "Sass",
+      },
+      {
+        image: "/assets/images/jira.svg",
+        title: "Jira",
+      },
+    ]
+  },
+  {
+    title: "The Soft Arsenal",
+    skills: ["Authenticity", "Language Proficiency","Collaboration", "Adaptability", "Problem-Solving", "Time Management", "Attention to Detail", "Lifelong Learning", "Curiosity", "Empathy", "Resilience"],
+  },
+];
+
+export default skillsData;
