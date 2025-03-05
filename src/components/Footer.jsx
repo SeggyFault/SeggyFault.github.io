9fffb03d7de62f3320e5ced86fb691e9f2e88315
sggyflt <sgflt88@gmail.com>
2025-03-05 12:42:19 +0100

    chore: update copyright year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="flex items-center justify-center py-10 text-white bg-black">
-      <span>© 2024 Oleksandra Masiukevych. All Rights Reserved.</span>
+      <span>© 2025 Oleksandra Masiukevych. All Rights Reserved.</span>
     </footer>
   );
 };
