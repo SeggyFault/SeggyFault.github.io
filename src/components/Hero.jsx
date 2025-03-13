b2e6a02fc97b7ad21d137d0685df84dd2691ab63
sggyflt <sgflt88@gmail.com>
2025-03-13 15:52:48 +0100

    feat: add mobile menu functionality

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const Hero = () => {
   return (
-    <div className="flex flex-col gap-4 sm:items-start justify-start w-full max-w-[1140px] my-6 md:my-24 mx-auto px-5 z-10 relative">
+    <div className="flex flex-col gap-4 sm:items-start justify-start w-full max-w-[1140px] my-6 md:my-24 mx-auto px-5 relative">
       
       <h1 className="md:leading-[80px] text-white uppercase leading-[50px] text-6xl md:text-8xl font-russo">Because the hard reality was <span className="text-red-600">I gotta get a job*</span><p className="text-xs italic text-white font-extralight font-lato">Quote from Major Payne (1995), Universal Pictures</p></h1>
       <p className="text-xl text-white md:text-2xl font-extralight font-lato">Portfolio of a <span className="font-bold">Dev Cadet Sasha</span>, the kind of genius who can code a missile’s trajectory and design the explosion to look like modern art!</p>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { MdMenu } from "react-icons/md";
+import { MdMenu, MdClose } from "react-icons/md";
 
 const Navigation = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="flex items-center justify-between w-full max-w-[1140px] my-0 mx-auto px-5 py-5 z-10 relative">
       <a href="/">
@@ -11,8 +17,11 @@ const Navigation = () => {
         className="w-12"/>
       </a>
 
-      <button className="text-5xl text-white md:hidden"><MdMenu/></button>
+      <button className="text-5xl text-white md:hidden z-[999]" onClick={toggleMenu}>
+        { isMenuOpen ? <MdClose className="fixed right-5 top-5" /> : <MdMenu /> }
+      </button>
 
+      {/* Desktop Menu */}
       <ul className="hidden w-1/2 md:flex md:items-center md:justify-between">
         <li className="text-base text-white uppercase font-lato hover:underline">
           <a href="#weapons">Weapons</a>
@@ -28,6 +37,27 @@ const Navigation = () => {
           <a href="#contact">Contact</a>
         </li>
       </ul>
+
+      {/* Mobile Menu */}
+      {isMenuOpen && (
+        <ul className="fixed top-0 left-0 w-full h-full z-[10] bg-[#161616] flex flex-col items-center gap-16 py-16 md:hidden">
+          <li className="text-5xl text-white uppercase font-lato hover:underline">
+            <a href="#weapons" onClick={toggleMenu}>Weapons</a>
+          </li>
+          <li className="text-5xl text-white uppercase font-lato hover:underline">
+            <a href="#missions" onClick={toggleMenu}>Missions</a>
+          </li>
+          <li className="text-5xl text-white uppercase font-lato hover:underline">
+            <a href="#about" onClick={toggleMenu}>About</a>
+          </li>
+          <li className="text-5xl text-white uppercase font-lato hover:underline">
+            <Link to="/blog" onClick={toggleMenu}>Blog</Link>
+          </li>
+          <li className="text-5xl text-white uppercase font-lato hover:underline">
+            <a href="#contact" onClick={toggleMenu}>Contact</a>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 };
